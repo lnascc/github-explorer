a7255dd82f28a77581e3207cb25cbcf6216a34ec
Larissa Nascimento <larissa.nascc15@gmail.com>
2022-02-18 18:22:33 -0300

    usando api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,28 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => { // dois parametros, o primeiro é a função que será executada e o segundo é quando será executada.
+    fetch('https://api.github.com/users/lnascc/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, []) 
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />   
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} /> 
+        })}
+          
       </ul>
     </section>
   )
